e899011c89ee872a31fc45c36b3d2998ea8df350
User510991 <131642525+User510991@users.noreply.github.com>
2024-12-31 15:29:03 +0100

    Create Description_et_données_brutes.py

---
+++ pages/Description_et_données_brutes.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+import plotly.graph_objects as go
+import numpy as np
+import plotly.express as px
+
+# Assuming 'df' is your DataFrame (replace with your actual DataFrame)
+# Example DataFrame (replace with your actual data)
+
+st.title("Statistiques Descriptives")
+
+# Sidebar for table selection
+selected_tables_raw = st.sidebar.multiselect("Choisir les tables", df.columns)
+reverse_selected_tables =st.sidebar.checkbox("Toutes les variables sauf celles sélectionnées")
+if len(selected_tables_raw):
+  if reverse_selected_tables:
+    selected_tables =[col for col in df.columns if col not in selected_tables_raw]
+  else:
+    selected_tables=selected_tables_raw
+ 
+# Sidebar for showing all data
+show_all_table= st.sidebar.checkbox("choisir toutes les variables")
+if show_all_table:
+  selected_tables=df.columns
+show_all_data= st.sidebar.checkbox("Afficher les données brutes")
+# Main content area
+if len(selected_tables):
+        st.write("### Statistiques descriptives")
+        st.write(df[list(selected_tables)].describe())  # Display basic descriptive statistics
+else:
+    st.write("Veuillez sélectionner au moins une colonne pour afficher les statistiques descriptives.")
+if show_all_data:
+    st.write("### Données brutes")
+    st.write(df[list(selected_tables)])
