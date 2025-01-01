9bc285bb0d6468293e4488281625f73c729cbbb9
User510991 <131642525+User510991@users.noreply.github.com>
2025-01-01 14:42:32 +0100

    Update 07_Métadonnées.py

--- pages/07_Métadonnées.py
+++ pages/07_Métadonnées.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import openpyxl
+import requests
+from openpyxl import load_workbook
 
 # Titre de l'application Streamlit
 st.title("Afficher un tableau Excel sur Streamlit")
@@ -8,8 +10,28 @@ st.title("Afficher un tableau Excel sur Streamlit")
 
 
 
+# URL du fichier
+url = "https://raw.githubusercontent.com/User510991/ISSEA_MES_Project/main/Livre.xlsx"
+
+# Télécharger le fichier et l'enregistrer localement
+response = requests.get(url)
+if response.status_code == 200:
+    with open("Livre.xlsx", "wb") as file:
+        file.write(response.content)
+    print("Fichier téléchargé avec succès.")
+else:
+    print(f"Échec du téléchargement. Code HTTP : {response.status_code}")
+    exit()
+
+# Charger le fichier avec openpyxl
+try:
+    workbook = load_workbook("Livre.xlsx")
+    print("Workbook chargé avec succès.")
+except Exception as e:
+    print(f"Erreur lors du chargement du fichier : {e}")
+        
+
     # Charger le fichier Excel avec openpyxl
-workbook = openpyxl.load_workbook("https://raw.githubusercontent.com/User510991/ISSEA_MES_Project/main/Livre.xlsx")
 
     # Sélectionner la première feuille (ou une feuille spécifique)
 sheet = workbook.active  # Ou sheet = workbook['Nom_de_la_feuille']
