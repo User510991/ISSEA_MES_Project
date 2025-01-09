7b840e177ca60122a2226aa73337dde5cecb7134
User510991 <131642525+User510991@users.noreply.github.com>
2025-01-09 13:19:52 +0100

    Create setup.R

---
+++ setup.R
@@ -0,0 +1,5 @@
+install.packages(c("moments", "urca", "readxl", "tseries", "ggplot2", "FinTS", 
+                   "caschrono", "nortest", "lmtest", "strucchange", "vars", 
+                   "dynlm", "tsDyn", "dLagM", "dynamac", "TSstudio", "ARDL", 
+                   "TSA", "car", "nardl", "CPAT", "systemfit", "AER", "foreign", 
+                   "xtable", "stargazer", "timeSeries", "Hmisc", "texreg", "forecast"))
