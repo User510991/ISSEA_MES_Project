b962e121eb55b95d4d1eb889da6ace9ed3752fa6
User510991 <131642525+User510991@users.noreply.github.com>
2025-01-11 11:57:03 +0100

    Update 06_Simulations.py

--- pages/06_Simulations.py
+++ pages/06_Simulations.py
@@ -25,7 +25,7 @@ def remove_column_spaces(df):
     Retourne :
     - pandas.DataFrame avec les noms de colonnes nettoyés
     """
-    df.columns = df.columns.str.strip()
+    df.columns = df.columns.str.strip().str.lower()
     return df
 # Exécuter le script R pour installer les packages
 #ro.r(install_r_packages)
