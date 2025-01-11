9f0998e57c04b1e2fb8abe97498d2b73f347b2b8
User510991 <131642525+User510991@users.noreply.github.com>
2025-01-11 12:17:13 +0100

    Update 06_Simulations.py

--- pages/06_Simulations.py
+++ pages/06_Simulations.py
@@ -153,13 +153,13 @@ if selected_vars:
   df_filled[["inflat","chom"]]=df_filled[["Inflat","Chomage"]]
   df_init1[["inflat","chom"]]=df_init1[["Inflat","Chomage"]]
 
-  col_names=[i for i in df_filled.columns if i not in ["FBCF","Imp_renouv","bal_ext_BS ","Inflat"]]
+  col_names=[i for i in df_filled.columns if i not in ["FBCF","Imp_renouv","bal_ext_BS ","Inflat","Chomage"]]
   for vari in col_names:
     for i in range(1,6):
       df_filled=create_lag_column(df_filled, vari, i)
       df_init1=create_lag_column(df_init1, vari, i)
 
-  col_names=[i for i in df_filled.columns if i not in ["FBCF","Imp_renouv","bal_ext_BS ","Inflat"]]
+  col_names=[i for i in df_filled.columns if i not in ["FBCF","Imp_renouv","bal_ext_BS ","Inflat","Chomage"]]
   df_a=df_init1[col_names]#remove_column_spaces(df_init1[col_names])
   df_t=df_filled[col_names]#remove_column_spaces(df_filled[col_names])
   st.dataframe(df_t)
